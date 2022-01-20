a46b536207a1169bbb4af4530ed2218a9532d50f
Steve Arch <sarch@cloudbees.com>
2022-01-20 16:34:01 +0000

    Handle multi-term delimiters

--- src/main/java/com/cloudbees/fm/jenkins/ui/AuditLogMessage.java
+++ src/main/java/com/cloudbees/fm/jenkins/ui/AuditLogMessage.java
@@ -32,18 +32,24 @@ import java.util.List;
  * A parsed/formatted version of an Audit Log message, ready for rendering in the UI
  */
 public class AuditLogMessage {
-    private static final String TERM = "<audit_log_targeting_term>";
-    private static final String DELIMITER = "<audit_log_targeting_delimiter>";
+    private static final String TARGETING_TERM = "<audit_log_targeting_term>";
+    private static final String TARGETING_DELIMITER = "<audit_log_targeting_delimiter>";
+    private static final String MULTI_TERM = "<audit_log_multi_term>";
+    private static final String MULTI_DELIMITER = "<audit_log_multi_delimiter>";
+    private static final String MULTI_TERM_END = "<audit_log_multi_term_end>";
 
     private final String mainMessage;
 
     private final List<String> subItems;
 
     public AuditLogMessage(String message) {
-        String[] parts = message.split(TERM);
+        // Total hack - for the moment, treat the two different types of terms and delimiters the same
+        message = message.replace(MULTI_TERM, TARGETING_TERM).replace(MULTI_DELIMITER, TARGETING_DELIMITER).replace(MULTI_TERM_END, "");
+
+        String[] parts = message.split(TARGETING_TERM);
         mainMessage = parts[0];
         if (parts.length > 1) {
-            subItems = Arrays.asList(parts[1].split(DELIMITER));
+            subItems = Arrays.asList(parts[1].split(TARGETING_DELIMITER));
         } else {
             subItems = Collections.emptyList();
         }
