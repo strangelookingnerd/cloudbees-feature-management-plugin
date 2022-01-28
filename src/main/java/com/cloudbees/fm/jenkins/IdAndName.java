a75db14dfd86eda96065a149c2c869233abdaee1
Steve Arch <sarch@cloudbees.com>
2022-01-28 16:25:17 +0000

    Comments about the delimiter breaking are no longer relevant, given we are using IDs

--- src/main/java/com/cloudbees/fm/jenkins/IdAndName.java
+++ src/main/java/com/cloudbees/fm/jenkins/IdAndName.java
@@ -28,7 +28,6 @@ package com.cloudbees.fm.jenkins;
  * A quick utility class to pass an ID and a name through jelly forms when selecting an option (and you are forced to use a string as the value)
  */
 public class IdAndName {
-    // This is probably not ideal, because on the off-chance a customer has an app/env with a name that contains ::, it'll break. But it's probably good enough for now
     private static final String DELIMITER = "::";
     private final String id;
 
